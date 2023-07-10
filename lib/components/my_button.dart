258da7626c6a2ac326748e1d769303c6f0c0242b
Ausaf Gill <121255256+ausafgill@users.noreply.github.com>
2023-07-10 16:05:54 +0500

    Update my_button.dart

--- lib/components/my_button.dart
+++ lib/components/my_button.dart
@@ -1,28 +1,6 @@
 import 'package:flutter/material.dart';
 
 
-// class MyButton extends StatefulWidget {
-//   final String title;
-//   final Color color;
-//   const MyButton({super.key, required this.title,  this.color= const Color(0xffa5a5a5)});
-//
-//   @override
-//   State<MyButton> createState() => _MyButtonState();
-// }
-//
-// class _MyButtonState extends State<MyButton> {
-//   @override
-//   Widget build(BuildContext context) {
-//     return Container(
-//       height: 80,
-//       decoration: BoxDecoration(
-//         shape: BoxShape.circle,
-//
-//       ),
-//
-//     );
-//   }
-// }
 
 class MyButton extends StatelessWidget {
   final String title;
